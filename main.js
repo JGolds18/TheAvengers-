4ded3c7d864560f6a09b2aa9c78289bcd1a925a4
JGolds18 <Jaime.Goldstein@protonmail.com>
2018-09-15 11:31:15 -0400

    adding a branch

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+let box = "bottle"
+console.log(box)
+
+box = "phone";
+console.log(box)
+
+box = "wire";
+console.log(box)
+
+let city = "Baltimore"
+console.log(city)
+
+city = "D.C."
+console.log(city)
+
+let languageA = "JavaScript";
+let languageB = "Java";
+
+let areTheyEqual = (languageA == languageB);
+console.log(areTheyEqual);
